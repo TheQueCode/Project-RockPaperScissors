86a5d3c0b2a79cdc2f72a042e68ca23c65af331e
TheQueCode <thequecode@gmail.com>
2025-04-29 15:51:42 -0500

    Add functions - Steps 1 thru 4

--- js/script.js
+++ js/script.js
@@ -9,5 +9,28 @@ declare winner
 
 
 function getComputerChoice(){
+  let computerChoice = Math.floor(Math.random() * 3 + 1);
+  if (computerChoice === 1) {
+    console.log("Rock");
+  } else if (computerChoice === 2){
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+}
 
+getComputerChoice();
+
+function getHumanChoice(){
+  let humanChoice = prompt("Enter Rock, Paper, or Scissors")
+  console.log(humanChoice);
+}
+
+getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  
 }
