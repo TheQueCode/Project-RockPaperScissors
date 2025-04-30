97d426f59aa196b3116e6b4b2dddc6a0987ea63f
TheQueCode <thequecode@gmail.com>
2025-04-30 09:27:55 -0500

    Add computer choice function

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function getComputerchoice(){
+  const computerChoice = Math.floor(Math.random() * 3 + 1);
+  if (computerChoice === 1){
+    console.log("Rock");
+  } else if (computerChoice === 2){
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+}
+
+getComputerchoice();
